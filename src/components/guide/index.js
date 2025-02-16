4718cec6d6a5e57698842400c882ad2c6edd7855
Ella Kitchen <z.yang110817@gmail.com>
2025-02-16 00:26:53 +0000

    Initial commit

---
+++ src/components/guide/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { isMobile } from '../../unit';
+import { transform } from '../../unit/const';
+import style from './index.less';
+
+export default class Guide extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isMobile: isMobile(),
+    };
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    if (this.state.isMobile) {
+      return null;
+    }
+    return (
+      <div style={{ display: this.state.isMobile ? 'none' : 'block' }}>
+        <div className={`${style.guide} ${style.right}`}>
+          <div className={style.up}>
+            <em style={{ [transform]: 'translate(0,-3px) scale(1,2)' }} />
+          </div>
+          <div className={style.left}>
+            <em style={{ [transform]: 'translate(-7px,3px) rotate(-90deg) scale(1,2)' }} />
+          </div>
+          <div className={style.down}>
+            <em style={{ [transform]: 'translate(0,9px) rotate(180deg) scale(1,2)' }} /></div>
+          <div className={style.right}>
+            <em style={{ [transform]: 'translate(7px,3px)rotate(90deg) scale(1,2)' }} />
+          </div>
+        </div>
+        <div className={`${style.guide} ${style.left}`}>
+          <div className={style.space}>SPACE</div>
+        </div>
+      </div>
+    );
+  }
+}
+
